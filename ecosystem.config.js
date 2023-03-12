74aa0868f3c4510c9128695005a9db5aa8eb3767
Bohdan <6alfabravo9@gmail.com>
2023-03-12 15:50:27 +0200

    fix: add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    script: 'npm start',
+
+  }],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '185.237.204.173',
+      ref: 'origin/main',
+      repo: 'https://github.com/Alfabravo228/nextjs.git',
+      path: '/home/root',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh-options': 'ForwardAgent=yes'
+    }
+  }
+};
